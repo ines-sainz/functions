359a25c813d8c6474e0fab1077db565b9b36b01a
Ines Sainz Ramon <isainz-r@c1r13s2.42madrid.com>
2024-06-06 09:45:23 +0200

    1 y 2 con normi

--- wdmatch.c
+++ wdmatch.c
@@ -6,7 +6,7 @@
 /*   By: isainz-r <isainz-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 18:39:39 by isainz-r          #+#    #+#             */
-/*   Updated: 2024/06/06 09:04:23 by isainz-r         ###   ########.fr       */
+/*   Updated: 2024/06/06 09:45:02 by isainz-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,18 @@ void	wdmatch(int argc, char **argv)
 {
 	int	i;
 	int	j;
-	int	len_argv1;
 
 	if (argc == 3)
 	{
 		i = 0;
 		j = 0;
-		len_argv1 = all_len(argv[1]);
 		while (argv[2][i])
 		{
 			if (argv[2][i] == argv[1][j])
-			{
 				j++;
-			}
 			i++;
 		}
-		if (j >= len_argv1)
+		if (j >= all_len(argv[1]))
 		{
 			i = 0;
 			while (argv[1][i])
